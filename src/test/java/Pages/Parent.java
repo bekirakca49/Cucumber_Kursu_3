75e64de1c30b4fdd2eed2cc0992f8e08bdbf7591
bekirakca49 <bekirakca0@gmailc.om>
2022-08-17 21:12:52 +0200

    frameWork calismasi1

---
+++ src/test/java/Pages/Parent.java
@@ -0,0 +1,17 @@
+package Pages;
+
+public class Parent {
+    sendKeysFunction(String element, string deger)
+    {
+        //beklemeyiYap
+        //kaydırmayı da yap
+        //kutucuğu temizle
+        //değeri gönder
+    }
+    clickFunction(String element)
+    {
+        //beklemeyiYap
+        //kaydırmayı da yap
+        //Click yap
+    }
+}
